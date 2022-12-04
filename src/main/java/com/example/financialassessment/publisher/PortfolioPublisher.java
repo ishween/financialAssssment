d451fbdb25de00e72570f70de19cbd58ef772353
Ishween Kaur <ishween.kaur@salesforce.com>
2022-12-04 15:18:06 -0800

    jotform changes added

--- src/main/java/com/example/financialassessment/publisher/PortfolioPublisher.java
+++ src/main/java/com/example/financialassessment/publisher/PortfolioPublisher.java
@@ -2,19 +2,23 @@ package com.example.financialassessment.publisher;
 
 import com.example.financialassessment.config.MessagingConfig;
 //import com.example.financialassessment.dto.Portfolio;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.amqp.core.MessageBuilder;
 import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -53,11 +57,58 @@ public class PortfolioPublisher {
 //        return "index_register";
     }
 
-    @PostMapping(value = "/register", consumes = {"application/json"})
-    public String submitForm(@RequestBody Map<String, Object> payload) {
+//    @PostMapping(value = "/register", consumes = {"application/json"})
+//    public String submitForm(@RequestBody Map<String, Object> payload) {
+//
+////        System.out.println(user);
+////        System.out.println(stockList);
+//        System.out.println("heyaaaa");
+//        System.out.println(payload);
+////        MessageProperties properties = new MessageProperties();
+////        properties.setContentType("application/json");
+////        MessageBuilder.withBody().andProperties(properties).build();
+////        template.convertAndSend(MessagingConfig.EXCHANGE, MessagingConfig.ROUTING_KEY, payload);
+//        try {
+////            template.setEncoding("UTF-8");
+//            template.convertAndSend(EXCHANGE, ROUTING_KEY, payload);
+//            return "register_success";
+//        }catch (Exception e){
+//            return "register_success";
+//        }
+//    }
+
+    @PostMapping(value = "/register", consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE})
+    public String submitForm(@RequestParam MultiValueMap<String, String > payload) {
 
 //        System.out.println(user);
 //        System.out.println(stockList);
+
+        System.out.println(payload.get("name[first]"));
+
+        System.out.println(payload.get("typea13"));
+
+        List<String> str = payload.get("typea13");
+        JSONArray array = new JSONArray(str);
+
+        System.out.println(array);
+        System.out.println(array.get(0));
+        System.out.println(array.get(0).toString());
+
+        JSONArray array1 = new JSONArray(array.get(0).toString());
+        System.out.println(array1);
+        System.out.println(array1.get(0));
+
+//        System.out.println(array1.get(0).toString());
+
+
+//        JSONObject object = new JSONObject(array.get(0));
+//        System.out.println(object.toString());
+
+//        System.out.println(array.getJSONArray(0));
+//        System.out.println(array.getJSONObject(0));
+//        System.out.println(array.getJSONArray(0).getJSONObject(0));
+
+
         System.out.println("heyaaaa");
         System.out.println(payload);
 //        MessageProperties properties = new MessageProperties();
