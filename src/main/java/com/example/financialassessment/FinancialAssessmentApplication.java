18557d71e4f2544e5801a9bfcc59561ccf6f872d
Ishween Kaur <ishween.kaur@salesforce.com>
2022-12-10 02:26:58 -0800

    added cloudmq

--- src/main/java/com/example/financialassessment/FinancialAssessmentApplication.java
+++ src/main/java/com/example/financialassessment/FinancialAssessmentApplication.java
@@ -3,12 +3,14 @@ package com.example.financialassessment;
 import com.example.financialassessment.service.PdfService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import javax.sql.DataSource;
 import java.util.HashMap;
 import java.util.Map;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class FinancialAssessmentApplication {
 
 	public static void main(String[] args) {
